29eea68fddffae2169925dd402a5ee4c98a93614
goran9999 <gutovicgoran@gmail.com>
2021-06-30 18:39:25 +0200

    AAA

--- public/client.js
+++ public/client.js
@@ -3,16 +3,139 @@ const socket=io.connect("http://localhost:4000");
 const roomInput=document.getElementById("roomInput");
 const createButton=document.getElementById("create");
 const activeRooms=document.getElementById("active-room");
-const joinButton=document.getElementById("join");
+const  joinButton=document.getElementById("join");
 const sendButton=document.getElementById("send");
 const chatDiv=document.getElementById("chat");
 const videoDiv=document.getElementById("video-div");
 const messageDiv=document.getElementById("message");
 const myVideo=document.getElementById("myVideo");
 const peerVideo=document.getElementById("peerVideo");
+const activeMeetings=document.getElementById("active-meetings");
+const peerVideos=document.getElementById("peer-videos");
+const camOff=document.getElementById("cameraOff");
+const micOff=document.getElementById("microphoneOff")
+const hangUp=document.getElementById("hang-up");
+const closeRoom=document.getElementById("close-room");
+const usernameDiv=document.getElementById("username-div");
+const chatLobby=document.getElementById("video-chat-lobby");
+const save=document.getElementById("save");
+const username=document.getElementById("username")
+const header=document.getElementById("header")
+let activeCalls=[];
+let peerVids=[];
+let joinButtons=[];
+let connections=[];
+let count=0;
+let order,connRoom,myRoom,myUsername;
+let cam=true,mic=true;
 var creator=true;
 var rtcPeerConnection=null;
 var userStream;
+let btns=[];
+//btns.push(joinButton);
+
+class Button{
+    constructor(button,counter){
+        this.button=button;
+        this.counter=counter;
+    }
+}
+camOff.addEventListener('click',function(){
+   cam=!cam;
+   userStream.getTracks()[0].enabled=cam;
+});
+
+micOff.addEventListener('click',function(){
+   mic=!mic;
+   userStream.getTracks()[1].enabled=mic;
+})
+
+closeRoom.addEventListener('click',function(){
+    socket.emit("closing-room",roomInput.value,myUsername);
+    creator=false;
+    myRoom=null;
+    var stream=myVideo.srcObject;
+    var tracks=stream.getTracks();
+     tracks.forEach(t => {
+        t.stop();
+    });
+     connections.forEach(conn=>{
+         conn.close();
+     })
+    peerVids.forEach(vid=>{
+        vid.remove();
+    })
+   myVideo.style.display="none";
+   videoDiv.style.display="none";
+   document.getElementById("videoButtons").style.display="none";
+   document.getElementById("chat-div").style.display="none";
+    document.getElementById("active-meetings").style.display="block";
+    document.getElementById("close-room").style.display="none";
+    chatLobby.style.display="block";
+    chatLobby.style.marginTop="250px";
+    chatLobby.style.marginLeft="450px";
+   
+})
+hangUp.addEventListener('click',function(){
+     //alert(connRoom);
+    //  if(creator){
+    //      socket.emit("hang-up",myVideo.srcObject.id,roomInput.value);
+    //  }
+    //else{
+    socket.emit("hang-up",myVideo.srcObject.id,connRoom,myUsername);
+    //}
+  myRoom=null;
+  var stream=myVideo.srcObject;
+  var tracks=stream.getTracks();
+   tracks.forEach(t => {
+      t.stop();
+  });
+   connections.forEach(conn=>{
+       conn.close();
+   })
+  peerVids.forEach(vid=>{
+      vid.remove();
+  })
+  myVideo.style.display="none";
+ videoDiv.style.display="none";
+ document.getElementById("videoButtons").style.display="none";
+ document.getElementById("chat-div").style.display="none";
+  document.getElementById("active-meetings").style.display="block";
+  chatLobby.style.display="block";
+  chatLobby.style.marginTop="200px";
+  chatLobby.style.marginLeft="450px";
+});
+save.addEventListener("click",function(){
+    if(username.value==''){
+        alert("Please enter valid username!");
+    }
+    else{
+        myUsername=username.value;
+        username.value='';
+        socket.emit("username",myUsername);
+        
+    }
+});
+sendButton.addEventListener('click',function(){
+   if(messageDiv.value==''){
+       return;
+   }
+   let message=document.createElement('div');
+   message.id="text";
+   let p=document.createElement('p');
+   p.id="time";
+   var today=new Date();
+   var time=today.getHours()+":"+today.getMinutes();
+   p.innerHTML= time;
+   message.appendChild(p);
+   let p2=document.createElement('div');
+   p2.id="messa";
+   p2.innerHTML=messageDiv.value;
+   message.appendChild(p2);
+   chatDiv.appendChild(message);
+   socket.emit("message",myRoom,messageDiv.value,myUsername);
+   messageDiv.value='';
+})
 let iceServers = {
     iceServers: [
       { urls: "stun:stun.services.mozilla.com" },
@@ -20,12 +143,32 @@ let iceServers = {
     ],
   };
 createButton.addEventListener('click',function(){
-   if(roomInput.value==''){
+    activeMeetings.style.display="none";
+    //document.getElementById("video-div").style.display="grid-auto-flow:row";
+    document.getElementById("myVideo").style.display="block";
+    document.getElementById("video-div").style.display="grid";
+    hangUp.style.display="none";
+    document.getElementById("close-room").style.display="block";
+    if(roomInput.value==''){
        alert("Please enter valid room name!");
+   }
+   else if(myUsername==undefined){
+       alert("Please enter valid username!")
    }
    else{
        creator=true;
        socket.emit("create",roomInput.value);
+       myRoom=roomInput.value;
+       socket.on("creating-failed",function(exists){
+       if(exists===true){
+           alert("Room with this name alredy exists!");
+           roomInput.value="";
+           return;
+       }
+       chatLobby.style.display="none";
+       usernameDiv.style.display="none";
+       document.getElementById("videoButtons").style.display="inline";
+       document.getElementById("chat-div").style.display="block";
        navigator.getUserMedia({
            video:true,
            audio:false
@@ -38,19 +181,95 @@ createButton.addEventListener('click',function(){
        },function(err){
            console.log(err);
        });
+    });
    }
 });
 socket.on("created",function(roomName){
-  activeRooms.value=roomName;
+    //activeRooms.value=roomName;
+    //btns.push(myButton);
+       var newButton=document.createElement('button');
+       var newTextBox=document.createElement('p');
+       newButton.className=roomName;
+       let myButton=new Button(newButton,count);
+       order=count;
+       count=count+1;
+       newTextBox.id="active-room";
+       newButton.id="join";
+       newTextBox.innerHTML=roomName;
+       newButton.textContent='Join';
+       joinButtons.push(newButton);
+       activeCalls.push(newTextBox.innerText);
+
+       const meetingsList = document.getElementById('meetings-list');
+       const meeting = document.createElement('li');
+       meeting.appendChild(newTextBox);
+       meeting.appendChild(newButton);
+       meetingsList.appendChild(meeting);
+      
+       
+    //    activeMeetings.appendChild(newButton);
+    //    activeMeetings.appendChild(newTextBox);
+       newButton.onclick=click(myButton,count);
 });
-joinButton.addEventListener('click',function(){
+   function click(e,c){
+    e.button.addEventListener('click',function(){
+        if(myUsername==undefined){
+           alert("Please enter valid username!");
+       }
+      else{
+      activeMeetings.style.display="none";
+      //videoDiv.style.display="block";
+      videoDiv.style.visibility="inital";
+      closeRoom.style.display="none";
+      document.getElementById("myVideo").style.display="block";
+      document.getElementById("video-div").style.display="grid";
+      chatLobby.style.display="none";
+      usernameDiv.style.display="none";
+      creator=false;
+      order=c-1; 
+     
+      for(let i=0;i<peerVids.length;i++){
+          let bool=false;
+          for(let j=0;j<peerVids.length;j++){
+              if(peerVids[i].id==peerVids[j].id||peerVids[i]==undefined){
+                  bool=true;
+              }
+          }
+          if(bool==true){
+              peerVids[i].remove();
+          }
+      }
+      console.log("Redni broj dugmeta:"+c);
+      document.getElementById("videoButtons").style.display="inline";
+       document.getElementById("chat-div").style.display="block";
+      console.log("Broj tekst divova:"+activeCalls.length);
+      navigator.getUserMedia({
+          video:true,
+          audio:false
+        },function(stream){
+            userStream=stream;
+            connRoom=activeCalls[order];
+            myRoom=activeCalls[order];
+            socket.emit("ready",/*activeRooms.value*/activeCalls[order],socket.id);
+            myVideo.srcObject=stream;
+            myVideo.onloadedmetadata=function(e){
+                myVideo.play();
+            }
+        },function(err){
+            console.log(err);
+        });
+      }
+    });
+}
+/*for(var i=0;i<btns.length;i++){
+btns[i].addEventListener('click',function(){
   creator=false; 
   navigator.getUserMedia({
       video:true,
       audio:false
     },function(stream){
         userStream=stream;
-        socket.emit("ready",activeRooms.value,socket.id);
+        socket.emit("ready",activeRooms.value,activeCalls[i].value,socket.id);
         myVideo.srcObject=stream;
         myVideo.onloadedmetadata=function(e){
             myVideo.play();
@@ -59,8 +278,10 @@ joinButton.addEventListener('click',function(){
         console.log(err);
     });
 });
+}*/
 socket.on("ready",function(socketid,roomSize){
     rtcPeerConnection=new RTCPeerConnection(iceServers);
+    connections.push(rtcPeerConnection);
     rtcPeerConnection.ontrack=OnTrack;
     rtcPeerConnection.addTrack(userStream.getTracks()[0],userStream);
     //rtcPeerConnection.addTrack(userStream.getTracks()[1],userStream);
@@ -79,7 +300,7 @@ socket.on("ready",function(socketid,roomSize){
                 rtcPeerConnection.createOffer().then((offer)=>{
                     rtcPeerConnection.setLocalDescription(offer);
                     console.log(offer);
-                    socket.emit("offer",offer,activeRooms.value,socketid,socket.id,type);
+                    socket.emit("offer",offer,/*activeRooms.value*/activeCalls[order],socketid,socket.id,type);
                 });
             
     }
@@ -90,9 +311,13 @@ socket.on("offer",async function(offer,socketid2){
         if(!creator){
             let answ;
            rtcPeerConnection=new RTCPeerConnection(iceServers);
+           connections.push(rtcPeerConnection);
             // rtcPeerConnection.addTrack(userStream2.getTracks()[1],userStream2);
             rtcPeerConnection.onicecandidate=OnIceCandidate2;
              rtcPeerConnection.ontrack=OnTrack;
+             rtcPeerConnection.onremovestream=function(){
+                 alert("Track removed")
+             }
                 rtcPeerConnection.addTrack(userStream.getTracks()[0],userStream);
                  rtcPeerConnection.setRemoteDescription(offer);
                await  rtcPeerConnection.createAnswer().then(async(answer)=>{  
@@ -100,7 +325,7 @@ socket.on("offer",async function(offer,socketid2){
                await rtcPeerConnection.setLocalDescription(answer);
                 }).then(async()=>{
                     console.log(answ);
-               await socket.emit("answer",answ,activeRooms.value,socketid2)
+               await socket.emit("answer",answ,/*activeRooms.value*/activeCalls[order],socketid2)
                 });
         }
    
@@ -108,11 +333,106 @@ socket.on("offer",async function(offer,socketid2){
 socket.on("answer",function(answer,roomSize){   
     console.log(answer);
         rtcPeerConnection.setRemoteDescription(answer);
+        
 });
 socket.on("candidate",function(candidate){
     const iceCandidate=new RTCIceCandidate(candidate);
     rtcPeerConnection.addIceCandidate(iceCandidate);
 });
+socket.on("hang-up",function(videoID){
+     var currentPeerDiv=document.getElementById(videoID);
+     //alert(videoID+"will be removed");
+     if(currentPeerDiv!=null)
+     currentPeerDiv.remove();
+     peerVids.forEach(vid=>{
+         if(vid.id==videoID){
+             //peerVids.remove(vid);
+             
+         }
+     })
+})
+socket.on("force-quit",function(roomName){
+   // alert("Admin closed room!");
+    let j;
+    for(let i=0;i<activeCalls.length;i++){
+        if(activeCalls[i]==roomName){
+            j=i;
+            activeCalls[i].remove();
+            //joinButtons[i].remove();
+        }
+    }
+    for(let i=0;i<joinButtons.length;i++){
+        if(joinButtons[i].className==roomName){
+            joinButtons[i].remove();
+        }
+    }
+    //activeMeetings[j].remove();
+    /*if(joinButtons[j]!=undefined){
+    joinButtons[j].style.display="none";
+    }*/
+    alert(myRoom);
+    if(myRoom==roomName){
+    hangUp.click();
+    myRoom=null;
+    }
+    if(myRoom==roomName){
+    document.getElementById("videoButtons").style.display="none";
+    document.getElementById("chat-div").style.display="none";
+    }
+})
+socket.on("message",function(mess,username){
+    let message=document.createElement('div');
+   message.id="text2";
+   let p=document.createElement('p');
+   p.id="time";
+   var today=new Date();
+   var time=today.getHours()+":"+today.getMinutes();
+   p.innerHTML= time;
+   message.appendChild(p);
+   let p2=document.createElement('div');
+   p2.id="messa";
+   p2.innerHTML=username.bold()+"\n"+mess;
+   message.appendChild(p2);
+   chatDiv.appendChild(message);
+   
+  
+
+})
+socket.on("active-rooms",function(element){
+    var newButton=document.createElement('button');
+    var newTextBox=document.createElement("p");
+    let myButton=new Button(newButton,count);
+    order=count;
+    count=count+1;
+    newTextBox.id="active-room";
+    newButton.id="join";
+    newTextBox.innerHTML=element;
+    newButton.textContent='Join';
+    activeCalls.push(newTextBox.innerText);
+    const meetingsList = document.getElementById('meetings-list');
+       const meeting = document.createElement('li');
+       meeting.appendChild(newTextBox);
+       meeting.appendChild(newButton);
+       meetingsList.appendChild(meeting);
+       newButton.onclick=click(myButton,count);
+    // activeMeetings.appendChild(newButton);
+    // activeMeetings.appendChild(newTextBox);
+});
+socket.on("same-username",function(user){
+    username.value="";
+    alert("Username "+user+" alredy exists!");
+})
+socket.on("valid-username",function(user){
+    const username=document.createElement('div');
+    var user2= user.toUpperCase();
+    username.innerHTML=user2;
+    header.appendChild(username);
+    username.style.float="right";
+    username.style.marginRight="15px";
+    username.style.marginTop="30px";
+    username.style.fontSize="25px";
+    username.style.color="green";
+})
 
 function OnIceCandidate(event){
     if(event.candidate){
@@ -121,15 +441,23 @@ function OnIceCandidate(event){
 }
 function OnIceCandidate2(event){
     if(event.candidate){
-    socket.emit("candidate",event.candidate,activeRooms.value);
+    socket.emit("candidate",event.candidate,/*activeRooms.value*/activeCalls[order]);
     }
 }
 function OnTrack(event){
+    var peerDiv=document.createElement('div');
     var peerVid=document.createElement('video');
-    videoDiv.appendChild(peerVid);
+    peerDiv.appendChild(peerVid);
+    peerVids.push(peerVid);
+    peerVid.id=event.streams[0].id
+    peerVid.className="peerVideo";
+    peerDiv.className="peerDiv";
+    //videoDiv.appendChild(peerDiv);
+    peerVideos.appendChild(peerDiv);
     peerVid.srcObject=event.streams[0];
     peerVid.onloadedmetadata=function(e){
         peerVid.play();
+       
     }
 
 }
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express=require("express");
 const app=express();
+var activeRooms=[];
+var usernames=[];
 const server=app.listen(4000,()=>{
     console.log("Server is listening on port 4000...")
 });
@@ -8,12 +10,76 @@ app.use(express.static("public"));
 const io=socket(server);
 let counter=0;
 io.on("connection",function(socket){
+    if(activeRooms.length>0){
+    activeRooms.forEach(room => {
+        console.log(room);
+    });
+}
     console.log("New connection:"+socket.id);
-    
+    if(activeRooms.length>0){
+        activeRooms.forEach(element => {
+         if(element!=null){
+         socket.emit("active-rooms",element);
+         }
+     });
+ }
+    socket.on("hang-up",function(videoId,roomName,myUsername){
+        socket.leave(roomName);
+        usernames.forEach(u=>{
+            if(u===myUsername){
+                u="";
+            }
+        })
+        socket.broadcast.to(roomName).emit("hang-up",videoId);
+       
+    });
+    socket.on("closing-room",function(roomName,myUsername){
+        for(let i=0;i<activeRooms.length;i++){
+            if(activeRooms[i]==roomName){
+                console.log("Soba koja ce biti ugasena "+activeRooms[i]);
+                activeRooms[i]=null;
+            }
+        }
+        usernames.forEach(u=>{
+            if(u===myUsername){
+                u="";
+            }
+        })
+        //socket.broadcast.to(roomName).emit("force-quit",roomName);
+        socket.broadcast.emit("force-quit",roomName);
+        
+    })
+    socket.on("message",function(roomName,message,username){
+        socket.broadcast.to(roomName).emit("message",message,username);
+    })
     socket.on("create",function(roomName){
+        let exists=false;
+        activeRooms.forEach((element)=>{
+            if(element===roomName)
+              exists=true;
+            })
+        socket.emit("creating-failed",exists);
+        if(exists===true){
+            return;
+        }
+        activeRooms.push(roomName);
         socket.join(roomName);
        socket.broadcast.emit("created",roomName);
     });
+    socket.on("username",function(myUsername){
+        var bool=false;
+        usernames.forEach(username=>{
+            if(username===myUsername){
+                bool=true;
+                socket.emit("same-username",username);
+            }
+        })
+        if(bool==false){
+            socket.emit("valid-username",myUsername);
+            usernames.push(myUsername);
+        }
+
+    })
     socket.on("ready",function(roomName,socketid){
         var rooms=io.sockets.adapter.rooms;
         var room=rooms.get(roomName);
@@ -23,6 +89,7 @@ io.on("connection",function(socket){
             }else{
                 console.log("Room does not exist!");
             }
+            
     
     });
     socket.on("candidate",function(candidate,roomName){
@@ -49,5 +116,4 @@ io.on("connection",function(socket){
     socket.on("answerHere",function(){
         socket.broadcast.to(roomName).emit("answerHere");
     })
-    
 });
